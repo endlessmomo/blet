ff2646a63c058957a9933cb7f3c24e414c9b3569
endelssmomo <soohyuk96@naver.com>
2024-01-26 13:39:22 +0900

    [build] : 모임통장 blet 초기 환경 설정

---
+++ src/main/java/yuki/blet/BletApplication.java
@@ -0,0 +1,13 @@
+package yuki.blet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BletApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BletApplication.class, args);
+  }
+
+}
---
+++ src/test/java/yuki/blet/BletApplicationTests.java
@@ -0,0 +1,13 @@
+package yuki.blet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BletApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
